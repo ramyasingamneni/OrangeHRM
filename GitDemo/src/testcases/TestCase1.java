3f87bc778837335cf8aae4252793e30fb0247043
ramya <ramya@RAMYA>
2024-01-22 15:29:19 +0530

    Test  Cases

---
+++ GitDemo/src/testcases/TestCase1.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class TestCase1 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("TestCase 1");
+
+	}
+
+}
---
+++ GitDemo/src/testcases/TestCase2.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class TestCase2 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("TestCase 2");
+
+	}
+
+}
